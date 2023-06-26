93d0282284d2533f4d7117aca704ba944ad44b8e
Anthony Aromin <menekoiskawaii@gmail.com>
2023-06-26 08:40:02 +0800

    Auto-set PUV location

--- app/src/main/java/com/example/puvtrackingsystem/PUVDetailsActivity.kt
+++ app/src/main/java/com/example/puvtrackingsystem/PUVDetailsActivity.kt
@@ -1,6 +1,9 @@
 package com.example.puvtrackingsystem
 
 import HttpGetRequestAsyncTask
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -11,10 +14,21 @@ import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.example.puvtrackingsystem.classes.BufferTime
+import com.example.puvtrackingsystem.classes.Coordinates
 import com.example.puvtrackingsystem.classes.PUV
 import com.example.puvtrackingsystem.constants.getStopNodes
+import com.example.puvtrackingsystem.utils.calculateDistance
 import com.example.puvtrackingsystem.utils.calculateTravelTime
+import com.example.puvtrackingsystem.utils.isLocationEnabled
+import com.example.puvtrackingsystem.utils.requestEnableLocation
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.OnTokenCanceledListener
 import com.google.gson.Gson
 import java.util.Calendar
 
@@ -28,8 +42,11 @@ class PUVDetailsActivity : AppCompatActivity() {
     private var currentPuv: PUV? = null
     private var bufferTimes: Array<BufferTime>? = null
     private var puvs: Array<PUV>? = null
+    private var currentLocation: Coordinates? = null
     private var currentStopNode: String = "Ambiong"
     private lateinit var puvDataScheduler: Handler
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var puvHasInitialized = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +56,8 @@ class PUVDetailsActivity : AppCompatActivity() {
         destinationSpinner = findViewById(R.id.destination_spinner)
         passengersTV = findViewById(R.id.passengers_tv)
         etaTV = findViewById(R.id.eta_tv)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         ArrayAdapter.createFromResource(
             this,
@@ -86,6 +105,8 @@ class PUVDetailsActivity : AppCompatActivity() {
             override fun onNothingSelected(parent: AdapterView<*>?) { }
         }
 
+        fromSpinner.isEnabled = false
+
         destinationSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
                 parent: AdapterView<*>?,
@@ -101,8 +122,39 @@ class PUVDetailsActivity : AppCompatActivity() {
             override fun onNothingSelected(parent: AdapterView<*>?) { }
         }
 
+        requestLocation()
         getBufferTimes()
         schedulePUVDataUpdates()
+    }
+
+    private fun requestLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ) return
+
+        if (!isLocationEnabled(this)) {
+            requestEnableLocation(this)
+            finish()
+            return
+        }
+
+        fusedLocationClient
+            .getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, object : CancellationToken() {
+                override fun onCanceledRequested(p0: OnTokenCanceledListener) = CancellationTokenSource().token
+
+                override fun isCancellationRequested() = false
+            })
+            .addOnSuccessListener { location: Location? ->
+                if (location == null) {
+                    Toast
+                        .makeText(this, "Could not get location. Turn on location services and internet :)", Toast.LENGTH_LONG)
+                        .show()
+                    return@addOnSuccessListener
+                };
+
+                currentLocation = Coordinates(location.latitude, location.longitude)
+                updatePUVDetails()
+            }
     }
 
     private fun schedulePUVDataUpdates() {
@@ -118,7 +170,30 @@ class PUVDetailsActivity : AppCompatActivity() {
     }
 
     private fun updatePUVDetails() {
-        if (bufferTimes == null || currentPuv == null) return
+        if (bufferTimes == null || currentPuv == null || currentLocation == null) return
+
+        if (!puvHasInitialized) {
+            var nearest = puvs!![0]
+            var nearestDistance = currentLocation!!.distanceTo(nearest.coordinates)
+            var idx = 0
+
+            for (i in 1 until puvs!!.size) {
+                val puv = puvs!![i]
+
+                val currentEvaluated = currentLocation!!.distanceTo(puv.coordinates)
+                if (currentEvaluated < nearestDistance) {
+                    nearest = puv
+                    nearestDistance = currentEvaluated
+                    idx = i
+                }
+            }
+
+            currentPuv = nearest
+            fromSpinner.setSelection(idx)
+
+            puvHasInitialized = true
+            fromSpinner.isEnabled = true
+        }
 
         val cal = Calendar.getInstance()
         val currentHour = cal.get(Calendar.HOUR_OF_DAY)
