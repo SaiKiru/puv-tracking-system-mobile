2d0d0eb82ca0633dfc8e7e8d68351c6bf36f39e9
Anthony Aromin <menekoiskawaii@gmail.com>
2023-07-15 14:39:35 +0800

    Redesign PUV card fragment

--- app/src/main/java/com/example/puvtrackingsystem/PuvCardFragment.kt
+++ app/src/main/java/com/example/puvtrackingsystem/PuvCardFragment.kt
@@ -12,6 +12,8 @@ private const val ARG_PUV = "puv"
 
 class PuvCardFragment : Fragment() {
     private var puv: PUV? = null
+    private lateinit var view: View
+    private var listener: View.OnClickListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,15 +26,21 @@ class PuvCardFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_puv_card, container, false)
+        view = inflater.inflate(R.layout.fragment_puv_card, container, false)
 
         val speedTextTV: TextView = view.findViewById(R.id.speed_text_tv)
         val passengerTextTV: TextView = view.findViewById(R.id.passenger_text_tv)
 
-        speedTextTV.text = puv!!.speed.toString()
-        passengerTextTV.text = puv!!.passengersOnboard.toString()
+        speedTextTV.text = "${puv!!.speed} kph"
+        passengerTextTV.text = "${puv!!.passengersOnboard} passengers"
+
+        view.setOnClickListener { listener?.onClick(view) }
 
         return view
+    }
+
+    fun setOnClickListener(listener: View.OnClickListener) {
+        this.listener = listener
     }
 
     companion object {
